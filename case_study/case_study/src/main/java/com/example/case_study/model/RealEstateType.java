45a763647998662e4f3c09b65579098f87783c5a
C0924G1-TRANMINHNHAT <minhnhat1507@gmail.com>
2025-01-23 14:10:49 +0700

    baitap

---
+++ case_study/case_study/src/main/java/com/example/case_study/model/RealEstateType.java
@@ -0,0 +1,25 @@
+package com.example.case_study.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "real_estate_type")
+public class RealEstateType {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", columnDefinition = "INT")
+    private int id;
+
+    @NotNull(message = "Type name must not be null")
+    @Column(name = "type_name", columnDefinition = "ENUM('HOUSE', 'APARTMENT')", nullable = false)
+    private String typeName;
+}
---
+++ case_study/case_study/src/main/java/com/example/case_study/model/Role.java
@@ -0,0 +1,22 @@
+package com.example.case_study.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "role")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", columnDefinition = "INT")
+    private int id;
+
+    @Column(name = "name", columnDefinition = "VARCHAR(50)", unique = true, nullable = false)
+    private String name;
+}

f5f0b1e2c1e4cdbc179e4d229e6d9bd159f8e473
linh77564 <you@example.com>
2025-02-05 13:54:21 +0700

    up account

---
+++ case_study/case_study/src/main/java/com/example/case_study/controller/logout/LogoutController.java
@@ -0,0 +1,16 @@
+package com.example.case_study.controller.logout;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LogoutController {
+    @GetMapping("/logout")
+    public String logoutSuccessful() {
+        return "login/login";
+    }
+    @GetMapping("/admin")
+    public String adminLogoutSuccessful() {
+        return "redirect:/login";
+    }
+}
